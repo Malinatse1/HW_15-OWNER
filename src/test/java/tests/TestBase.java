d7de11febde297244896d62f06876738755fe810
bochk <natalya-maligon@mail.ru>
2023-01-13 11:25:48 +0300

    Add new code

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -8,8 +8,10 @@ public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = false;
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
     }
